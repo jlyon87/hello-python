2cb4cfbefabc4bbc3f3a4f8879db0dd2b69a6c51
jlyon87 <jml6487@gmail.com>
2020-06-20 21:20:46 -0500

    done for the evening

--- src/git-stuff.py
+++ src/git-stuff.py
@@ -1,24 +1,22 @@
+# pip install gitpython
 from git import Repo
 import os
-print(os.getcwd())
 
+# Init repo object from current project dir (which has .git dir)
 repo = Repo(os.getcwd())
 assert not repo.bare
-
-print(repo)
-print(repo.is_dirty())
-print(repo.untracked_files)
 
 master_local = repo.commit("master")
 master_remote = repo.commit("origin/master")
 
-print(master_local)
-print(master_remote)
+print(
+    f"{str(master_remote)[0:8]}..{str(master_local)[0:8]}")
 
-diff_index = master_remote.diff(master_local)
-print(diff_index)
+# Take diff between origin/master and master
+diffs = master_remote.diff(master_local)
 
-for diff in diff_index:
-    print(diff.change_type)
-    print(diff.a_path)
-    print(diff.b_path)
+# List all the files in the diff
+for diff in diffs:
+    # path can safely use diff.a_path until the change_type is Rename (R)
+    path = diff.a_path if diff.change_type != "R" else f"{diff.a_path} => {diff.b_path}"
+    print(f"{diff.change_type} {path}")
