7becbf0473d651740631f6e6e2a959bf15b669ac
jlyon87 <jml6487@gmail.com>
2020-06-20 21:01:43 -0500

    add more local changes

--- src/git-stuff.py
+++ src/git-stuff.py
@@ -12,7 +12,11 @@ print(repo.untracked_files)
 master_local = repo.commit("master")
 master_remote = repo.commit("origin/master")
 
+print(master_local)
+print(master_remote)
+
 diff_index = master_remote.diff(master_local)
+print(diff_index)
 
 for item in diff_index.iter_change_type('M'):
     print(item)
