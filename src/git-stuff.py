48a1e918e674309d9aeefe34395ef3ccf50ee9f6
jlyon87 <jml6487@gmail.com>
2020-06-20 20:56:58 -0500

    commit any local change

--- src/git-stuff.py
+++ src/git-stuff.py
@@ -4,8 +4,15 @@ print(os.getcwd())
 
 repo = Repo(os.getcwd())
 assert not repo.bare
+
 print(repo)
-
 print(repo.is_dirty())
-
 print(repo.untracked_files)
+
+master_local = repo.commit("master")
+master_remote = repo.commit("origin/master")
+
+diff_index = master_remote.diff(master_local)
+
+for item in diff_index.iter_change_type('M'):
+    print(item)
